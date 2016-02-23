ffa6d17872110612abdef679b4eeff488d516c7c
Bryce McDonnell <bryce@cricketscircle.com>
2016-02-22 23:09:57 -0500

    Include all data points

--- src/main/scala/kdd_regression.scala
+++ src/main/scala/kdd_regression.scala
@@ -19,36 +19,10 @@ import org.apache.log4j.Level
 
 object KDD {
 
-  def matchYesNo(vari: String) : Int = vari match {
-    case "yes" => 1
-    case "no"  => 0
-  }
-
-  def nephritisPoint(dataString: String) : LabeledPoint = {
-      val data = dataString.split("\t")
-      val patientTemp = data(0).replaceAll(",", ".").toDouble
-      val patientNausea = matchYesNo( data(1) )
-      val patientLumbarPain = matchYesNo( data(2) )
-      val patientUrinePushing = matchYesNo( data(3) )
-      val patientMicturitionPain = matchYesNo( data(4) )
-      val patientBurning = matchYesNo( data(5) )
-      val outcomeInflamation = matchYesNo( data(6) )
-      val outcomeNephritis  = matchYesNo( data(7) )
-
-      val vec = Vectors.dense(patientTemp,
-                              patientNausea,
-                              patientLumbarPain,
-                              patientUrinePushing,
-                              patientMicturitionPain,
-                              patientBurning
-                            )
-      LabeledPoint(outcomeNephritis, vec)
-  }
-
   def train(trainingData: org.apache.spark.rdd.RDD[LabeledPoint]) : LogisticRegressionModel = {
-    new LogisticRegressionWithLBFGS()
-      .setNumClasses(2)
-    //new LogisticRegressionWithSGD()
+    //new LogisticRegressionWithLBFGS()
+      //.setNumClasses(2)
+    new LogisticRegressionWithSGD()
       .run(trainingData)
   }
 
@@ -74,6 +48,9 @@ object KDD {
 
     val multMetrics = new MulticlassMetrics(predictionAndLabels)
     println(multMetrics.confusionMatrix)
+    multMetrics.labels.foreach(label =>
+      println(s"[Model Perf] $label precision:  ${multMetrics.precision(label)}"))
+    println(s"[Model Perf] weighted precision: ${multMetrics.weightedPrecision}")
     metrics
   }
 
@@ -83,6 +60,7 @@ object KDD {
       .format("com.databricks.spark.csv")
       .option("inferSchema", "true")
       .load("hdfs://spark3.thedevranch.net/classifications-datasets/kddcup.data_10_percent")
+      //.load("hdfs://spark3.thedevranch.net/classifications-datasets/kddcup.data")
 
       //.fit(df, ("C1" -> "transportProtoIndex"), ("C2" -> "protoIndex"))
     val mappers: List[(String, String)] = List(
@@ -93,36 +71,64 @@ object KDD {
      )
 
    var mutDataFrame : DataFrame = df
+   // TODO: this is inefficient
    val transformed = mappers.map{ m =>
      val strIdx = new StringIndexer()
      .setInputCol(m._1)
      .setOutputCol(m._2)
      mutDataFrame = transformIt(strIdx, mutDataFrame)
    }
-   //mutDataFrame.printSchema()
-   // casting to doubles is required for stuff not to blow up
+   //mutDataFrame.show()
+
    val modelData = mutDataFrame.selectExpr(
      "cast(CASE C41 WHEN 'normal.' THEN 0.0 ELSE 1.0 END as DOUBLE) as outcomeBin",
      "cast(C4 as DOUBLE) as C4",
+     "cast(C5 as DOUBLE) as C5",
+     "cast(C6 as DOUBLE) as C6",
+     "cast(C7 as DOUBLE) as C7",
+     "cast(C8 as DOUBLE) as C8",
+     "cast(C9 as DOUBLE) as C9",
+     "cast(C10 as DOUBLE) as C10",
+     "cast(C11 as DOUBLE) as C11",
+     "cast(C12 as DOUBLE) as C12",
+     "cast(C13 as DOUBLE) as C13",
+     "cast(C14 as DOUBLE) as C14",
+     "cast(C15 as DOUBLE) as C15",
+     "cast(C16 as DOUBLE) as C16",
+     "cast(C17 as DOUBLE) as C17",
+     "cast(C18 as DOUBLE) as C18",
+     "cast(C19 as DOUBLE) as C19",
+     "cast(C20 as DOUBLE) as C20",
+     "cast(C21 as DOUBLE) as C21",
+     "cast(C22 as DOUBLE) as C22",
+     "cast(C23 as DOUBLE) as C23",
+     "cast(C24 as DOUBLE) as C24",
+     "cast(C25 as DOUBLE) as C25",
+     "cast(C26 as DOUBLE) as C26",
+     "cast(C27 as DOUBLE) as C27",
+     "cast(C28 as DOUBLE) as C28",
+     "cast(C29 as DOUBLE) as C29",
+     "cast(C30 as DOUBLE) as C30",
+     "cast(C31 as DOUBLE) as C31",
+     "cast(C32 as DOUBLE) as C32",
+     "cast(C33 as DOUBLE) as C33",
+     "cast(C34 as DOUBLE) as C34",
+     "cast(C35 as DOUBLE) as C35",
+     "cast(C36 as DOUBLE) as C36",
+     "cast(C37 as DOUBLE) as C37",
+     "cast(C38 as DOUBLE) as C38",
+     "cast(C39 as DOUBLE) as C39",
+     "cast(C40 as DOUBLE) as C40",
      "transportProtoIndex",
      "protoIndex",
      "idunnoIndex"
    )
 
-   modelData.show()
+   //modelData.show()
 
    //NOTE: if the select statement from creating this df changes, update `rowToLabeledPoint`
    val splits = modelData
-     .map{row =>
-       //println( row.schema )
-       val vec = Vectors.dense(
-         row.getDouble(1),
-         row.getDouble(2),
-         row.getDouble(3),
-         row.getDouble(4)
-       )
-       LabeledPoint(row.getDouble(0), vec)
-     }
+     .map(row => transformRowToLabeledPoint(row) )
      .randomSplit(Array(0.8, 0.2), seed = 11L)
    val training = splits(0).cache()
    val test = splits(1)
@@ -140,48 +146,51 @@ object KDD {
   }
 
   def transformRowToLabeledPoint(row: Row) = LabeledPoint(row.getDouble(0),
-       Vectors.dense(
-         row.getDouble(1),
-         row.getDouble(2),
-         row.getDouble(3),
-         row.getDouble(4)
-      )
+    Vectors.dense(
+      row.getDouble(1),
+      row.getDouble(2),
+      row.getDouble(3),
+      row.getDouble(4),
+      row.getDouble(5),
+      row.getDouble(6),
+      row.getDouble(7),
+      row.getDouble(8),
+      row.getDouble(9),
+      row.getDouble(10),
+      row.getDouble(11),
+      row.getDouble(12),
+      row.getDouble(13),
+      row.getDouble(14),
+      row.getDouble(15),
+      row.getDouble(16),
+      row.getDouble(17),
+      row.getDouble(18),
+      row.getDouble(19),
+      row.getDouble(20),
+      row.getDouble(21),
+      row.getDouble(22),
+      row.getDouble(23),
+      row.getDouble(24),
+      row.getDouble(25),
+      row.getDouble(26),
+      row.getDouble(27),
+      row.getDouble(28),
+      row.getDouble(29),
+      row.getDouble(30),
+      row.getDouble(31),
+      row.getDouble(32),
+      row.getDouble(33),
+      row.getDouble(34),
+      row.getDouble(35),
+      row.getDouble(36),
+      row.getDouble(37),
+      row.getDouble(38),
+      row.getDouble(39),
+      row.getDouble(40)
+    )
   )
 
   def transformIt(strIdxer: StringIndexer, df: DataFrame) : DataFrame = {
     strIdxer.fit(df).transform(df)
-  }
-
-  def trainTestAndEvalOLD(sc : SparkContext) = {
-
-    // NOTE: dataset is @ https://archive.ics.uci.edu/ml/datasets/URL+Reputation
-    //val data = MLUtils.loadLibSVMFile(sc, "hdfs://spark3.thedevranch.net/classifications-datasets/diagnosis.data")
-
-    val diagnosisData = sc.textFile("hdfs://spark3.thedevranch.net/classifications-datasets/kddcup.data_10_percent")
-    // https://archive.ics.uci.edu/ml/machine-learning-databases/acute/
-    val nephritis = diagnosisData.map(row => nephritisPoint(row) )
-    val splits = nephritis.randomSplit(Array(0.8, 0.2))
-    val training = splits(0)//.cache()
-    val test = splits(1) //.cache()
-
-    println("===== TRAINING =====")
-    println("data set count: " + training.count)
-    val model = train(training)
-
-
-    // this syntax is not great. It indicates that index 1, has 2 categories
-    // FIXME - make sure categorical vars are used
-    val categoricalFeaturesInfo = Map[Int, Int]((1 -> 2), (2 -> 2))
-
-
-    println("===== PREDICT =====")
-    println("weights: " + model.weights)
-    val predictionAndLabels = predict(model, test)
-
-    // output some cases to see how well we did
-    //predictionAndLabels.take(5).foreach(println)
-
-    println("===== MODEL PERF =====")
-    modelPerformance(predictionAndLabels)
   }
 }
